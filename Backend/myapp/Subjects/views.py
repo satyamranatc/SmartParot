a28f7e2cde97579d51a4147b591e96aab04bd714
PrathamTawar <tawpra28@gmail.com>
2024-12-26 19:43:50 +0530

    added POST and GET in same function

--- Backend/myapp/Subjects/views.py
+++ Backend/myapp/Subjects/views.py
@@ -4,7 +4,7 @@ from rest_framework.response import Response
 from .models import Subjects
 from .serializers import SubjectSerializer
 
-@api_view(["GET"])
+@api_view(["GET", "POST"])
 def getSubjects(requests):
     
     if requests.method == "GET":
